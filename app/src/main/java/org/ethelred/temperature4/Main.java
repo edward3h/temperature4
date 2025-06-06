8861fd4f54b5ad418318a4f33e59b6d11d1004bf
Edward Harman <jaq+github@ethelred.org>
2025-06-05 22:39:03 -0400

    request logging

--- app/src/main/java/org/ethelred/temperature4/Main.java
+++ app/src/main/java/org/ethelred/temperature4/Main.java
@@ -5,11 +5,16 @@ import io.avaje.config.Configuration;
 import io.avaje.inject.BeanScope;
 import io.avaje.inject.spi.GenericType;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 import io.javalin.http.staticfiles.Location;
 import io.javalin.plugin.Plugin;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         BeanScope bs = BeanScope.builder().build();
         var updater = bs.get(SettingUpdater.class);
@@ -21,6 +26,16 @@ public class Main {
                     services.forEach(cfg::registerPlugin);
                     cfg.staticFiles.add("/static", Location.CLASSPATH);
                 })
+                .beforeMatched(Main::beforeRequest)
+                .afterMatched(Main::afterRequest)
                 .start(configuration.getInt("server.port", 8080));
+    }
+
+    private static void afterRequest(Context context) {
+        LOGGER.info("After {} {}", context.method(), context.path());
+    }
+
+    private static void beforeRequest(Context context) {
+        LOGGER.info("Before {} {}", context.method(), context.path());
     }
 }
