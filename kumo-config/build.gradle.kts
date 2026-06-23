de36746d4da382791114f6658ab8c301cd8b26c4
Edward Harman <jaq+github@ethelred.org>
2026-06-23 14:23:43 -0400

    build: scaffold kumo-config module

---
+++ kumo-config/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    application
+    id("com.diffplug.spotless") version "8.7.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.avaje:avaje-jsonb:3.14")
+    testImplementation(project(":kumo"))
+    testImplementation("com.google.truth:truth:1.4.5")
+}
+
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter("5.11.1")
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+application {
+    mainClass = "org.ethelred.kumoconfig.KumoConfigMain"
+}
+
+spotless {
+    java {
+        target("src/**/*.java")
+        importOrder()
+        removeUnusedImports()
+        palantirJavaFormat().formatJavadoc(true)
+        formatAnnotations()
+        licenseHeader("// (C) Edward Harman \$YEAR")
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "temperature4"
 include("app")
 include("kumo")
+include("kumo-config")
